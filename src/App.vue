c4138d34b28c10ad61f7d5a8b08e07bac6f8ca21
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2023-08-08 20:05:02 +0530

    Committing the Vue Kanban Getting Started video

---
+++ src/App.vue
@@ -0,0 +1,149 @@
+<script >
+  import {KanbanComponent, ColumnsDirective, ColumnDirective} from '@syncfusion/ej2-vue-kanban'
+  import {DataManager, ODataAdaptor} from '@syncfusion/ej2-data'
+  export default {
+    name: "App",
+    components: {
+      'ejs-kanban': KanbanComponent,
+      'e-columns': ColumnsDirective,
+      'e-column': ColumnDirective
+    },
+    data()
+    {
+      return{
+        kanbanData: new DataManager(
+          {
+            url:'https://ej2services.syncfusion.com/production/web-services/api/Kanban',
+            adaptor:new ODataAdaptor(),
+            crossDomain:true
+          }
+        ),
+  //       [
+  //       {
+  //     'Id': 1,
+  //     'Status': 'Open',
+  //     'Summary': 'Analyze the new requirements gathered from the customer.',
+  //     'Type': 'Story',
+  //     'Priority': 'Low',
+  //     'Tags': 'Analyze,Customer',
+  //     'Estimate': 3.5,
+  //     'Assignee': 'Nancy Davloio',
+  //     'RankId': 1
+  // },
+  // {
+  //     'Id': 2,
+  //     'Status': 'InProgress',
+  //     'Summary': 'Improve application performance',
+  //     'Type': 'Improvement',
+  //     'Priority': 'Normal',
+  //     'Tags': 'Improvement',
+  //     'Estimate': 6,
+  //     'Assignee': 'Andrew Fuller',
+  //     'RankId': 1
+  // },
+  // {
+  //     'Id': 3,
+  //     'Status': 'Testing',
+  //     'Summary': 'Arrange a web meeting with the customer to get new requirements.',
+  //     'Type': 'Others',
+  //     'Priority': 'Critical',
+  //     'Tags': 'Meeting',
+  //     'Estimate': 5.5,
+  //     'Assignee': 'Janet Leverling',
+  //     'RankId': 2
+  // },
+  // {
+  //     'Id': 4,
+  //     'Status': 'Close',
+  //     'Summary': 'Fix the issues reported in the IE browser.',
+  //     'Type': 'Bug',
+  //     'Priority': 'Release Breaker',
+  //     'Tags': 'IE',
+  //     'Estimate': 2.5,
+  //     'Assignee': 'Janet Leverling',
+  //     'RankId': 2
+  // },
+  // {
+  //     'Id': 5,
+  //     'Status': 'Open',
+  //     'Summary': 'Fix the issues reported by the customer.',
+  //     'Type': 'Bug',
+  //     'Priority': 'Low',
+  //     'Tags': 'Customer',
+  //     'Estimate': '3.5',
+  //     'Assignee': 'Steven walker',
+  //     'RankId': 1
+  // },
+  // {
+  //     'Id': 6,
+  //     'Status': 'InProgress',
+  //     'Summary': 'Arrange a web meeting with the customer to get the login page requirements.',
+  //     'Type': 'Others',
+  //     'Priority': 'Low',
+  //     'Tags': 'Meeting',
+  //     'Estimate': 2,
+  //     'Assignee': 'Michael Suyama',
+  //     'RankId': 1
+  // },
+  // {
+  //     'Id': 7,
+  //     'Status': 'Testing',
+  //     'Summary': 'Validate new requirements',
+  //     'Type': 'Improvement',
+  //     'Priority': 'Low',
+  //     'Tags': 'Validation',
+  //     'Estimate': 1.5,
+  //     'Assignee': 'Robert King',
+  //     'RankId': 1
+  // },
+  // {
+  //     'Id': 8,
+  //     'Status': 'Close',
+  //     'Summary': 'Login page validation',
+  //     'Type': 'Story',
+  //     'Priority': 'Release Breaker',
+  //     'Tags': 'Validation,Fix',
+  //     'Estimate': 2.5,
+  //     'Assignee': 'Laura Callahan',
+  //     'RankId': 2
+  //   }
+  // ],
+     cardSettings:{
+      contentField:"Summary",
+      headerField:"Id"
+     },
+     swimlaneSettings:{
+      keyField:"Assignee"
+     }
+    }
+  }
+}
+</script>
+
+<template>
+ <ejs-kanban :dataSource="kanbanData" keyField="Status" :cardSettings="cardSettings"
+                                      height="100%"
+                                      width="100%"
+                                      :swimlaneSettings="swimlaneSettings"
+                                      :enableTooltip="true"
+                                      >
+  <e-columns>
+    <e-column headerText="To Do" keyField="Open" maxCount="1"></e-column>
+    <e-column headerText="In Progress" keyField="InProgress"></e-column>
+    <e-column headerText="Review" keyField="Review" ></e-column>
+    <e-column headerText="Testing" keyField="Testing" minCount="3"></e-column>
+    <e-column headerText="Done" keyField="Close"></e-column>
+  </e-columns>
+ </ejs-kanban>
+</template>
+
+<style>
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-layouts/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-vue-kanban/styles/material.css';
+</style>
